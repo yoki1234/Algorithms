dd8e12b28ad0e30fa062e792c32ec76e615abdf5
akshay_ashok <akshayashokdj@gmail.com>
2022-11-08 18:24:39 +0530

    feat: insertionSorting

---
+++ sorting-algorithms/insertionSort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = numberToInsert;
+  }
+}
+
+const arr = [2, 3, -9, -7, 12];
+console.log(arr);
+insertionSort(arr);
+console.log(arr);
